4ce5dedf628fb45cd39e74c0466fedc7657fe697
Jason Mallard <jMallard404@gmail.com>
2014-07-22 18:50:08 -0300

    Messing with spawn range

--- forge/src/main/java/group4/radiationgame/common/RadiationHouse.java
+++ forge/src/main/java/group4/radiationgame/common/RadiationHouse.java
@@ -9,9 +9,11 @@ import java.util.Random;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.WeightedRandomChestContent;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.*;
@@ -50,19 +52,33 @@ public class RadiationHouse extends WorldGenDungeons{
 	public boolean generate(World world, Random random, int x, int y, int z)
     {
 		
-		y = 15;
-		//add requirements
-		//
+		int playerX = x;
+		int playerZ = z;
 		
 		if(generated){
 			return true;
 		}
-		//for testing
-		
+		String direction =  "Our intelligence has told us that the source of the radiation is  ";
 		//math is cool
 		//math.pow(-1, n) is an alternating 1, -1 to make more variation in just one line
-		x = x + (int) ((Math.pow(-1, random.nextInt(4)) * 100) + (Math.pow(-1, random.nextInt(4)) * random.nextInt(30))); 
-		z = z + (int) ((Math.pow(-1, random.nextInt(4)) * 100) + (Math.pow(-1, random.nextInt(4)) * random.nextInt(30)));
+		x = x + (int) ((Math.pow(-1, random.nextInt(4)) * 105) + (Math.pow(-1, random.nextInt(4)) * random.nextInt(50))); 
+		z = z + (int) ((Math.pow(-1, random.nextInt(4)) * 105) + (Math.pow(-1, random.nextInt(4)) * random.nextInt(50)));
+		
+		if (x - playerX > 0 && z - playerZ > 0){
+			direction += "North-east";
+		}else if(x - playerX > 0 && z - playerZ < 0){
+			direction += "North-west";
+		}else if(x - playerX < 0 && z - playerZ < 0){
+			direction += "South-west";
+		}else{
+			direction += "South-east";
+		}
+		
+		direction += " from your current position";
+		
+		Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(direction));
+		
+		y = 15;
 		
 		while(!world.canBlockSeeTheSky(x, y, z)){
 			y++;
